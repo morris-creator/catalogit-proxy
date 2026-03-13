443e5bbacfda3f0fd992596185126b025d73912b
morris-creator <morris@jameshowellfoundation.org>
2026-03-13 10:57:39 -0400

    Update collection.js

--- api/collection.js
+++ api/collection.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
 
     let allResults = [];
-    let nextURL = "https://api.catalogit.app/api/v1/collection";
+    let nextURL = "https://api.catalogit.app/api/v1/entries";
 
     while (nextURL) {
 
