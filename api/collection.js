df253e300aa89a89d61e615b4c0c8f0eff34a22b
morris-creator <morris@jameshowellfoundation.org>
2026-03-13 11:02:54 -0400

    Update collection.js

--- api/collection.js
+++ api/collection.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
 
     let allResults = [];
-   let nextURL = "https://api.catalogit.app/api/v1/orgs/16688/entries";
+    let nextURL = "https://api.catalogit.app/v1/orgs/16688/entries";
 
     while (nextURL) {
 
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         allResults = allResults.concat(data.results);
       }
 
-      nextURL = data.next; // CatalogIt provides the next page link
+      nextURL = data.next;
 
     }
 
