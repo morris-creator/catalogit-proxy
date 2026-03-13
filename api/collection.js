5c0b20efb1ea5c597ca1bced0b141a7a2f9612b0
morris-creator <morris@jameshowellfoundation.org>
2026-03-13 14:47:17 -0400

    Update collection.js

--- api/collection.js
+++ api/collection.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   try {
 
     let allResults = [];
